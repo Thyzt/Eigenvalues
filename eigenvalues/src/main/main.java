c2550056afd75e4947705460198826e68e62174d
Thyzt <128422446+Thyzt@users.noreply.github.com>
2024-12-10 23:06:32 -0500

    template created for graphics

--- eigenvalues/src/main/main.java
+++ eigenvalues/src/main/main.java
@@ -1,7 +1,24 @@
 package main;
 
+import javax.swing.JFrame;
+
 public class main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Eigenvalues");
+
+        GraphicsPanel graphicsPanel = new GraphicsPanel();
+        window.add(graphicsPanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        
+        graphicsPanel.startGraphicsThread();
     }
 }
